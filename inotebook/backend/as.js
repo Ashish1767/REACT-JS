8d5c1d4d7e1fd34987b41d32e7011b337c321176
Ashish1767 <122530284+Ashish1767@users.noreply.github.com>
2023-04-23 19:51:21 +0530

    Add files via upload

---
+++ inotebook/backend/as.js
@@ -0,0 +1,21 @@
+import pymongo
+import csv
+import zipfile
+
+# Connect to MongoDB
+client = pymongo.MongoClient("mongodb://localhost:27017/")
+db = client["mydatabase"]
+
+#Open the zip file containing the CSV file
+with zipfile.ZipFile("collection1.zip", "r") as zip_ref:
+    # Extract the CSV file
+    zip_ref.extractall()
+
+# Open the CSV file
+with open("collection1.csv", "r") as csvfile:
+    # Read the CSV file
+    reader = csv.DictReader(csvfile)
+
+    # Insert each row as a document in the collection
+    for row in reader:
+        db.collection1.insert_one(row)
